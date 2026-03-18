8dcec06d8d6e93dfcc9d005716ff8513f6fc9843
lux032 <linrui745420@gmail.com>
2026-03-18 15:13:02 +0800

    拆分优化入口代码

---
+++ app/src/main/java/com/lux032/plextosonosplayer/AppModels.kt
@@ -0,0 +1,56 @@
+package com.lux032.plextosonosplayer
+
+import com.lux032.plextosonosplayer.plex.PlexAlbum
+import com.lux032.plextosonosplayer.plex.PlexTrackStream
+import com.lux032.plextosonosplayer.sonos.SonosRoom
+
+enum class AppSection {
+    Home,
+    Artists,
+    AlbumDetail,
+    ArtistAlbums,
+    PlaybackDetail,
+    FavoriteCollection,
+    RecentAdded,
+    AllAlbums,
+    Settings,
+}
+
+enum class MessageTone {
+    Info,
+    Error,
+}
+
+data class MiniPlayerState(
+    val album: PlexAlbum,
+    val tracks: List<PlexTrackStream>,
+    val currentIndex: Int,
+    val room: SonosRoom,
+    val isPaused: Boolean,
+)
+
+enum class PlayerIcon {
+    Back,
+    Previous,
+    Play,
+    Pause,
+    Next,
+}
+
+enum class FavoriteIconStyle {
+    Outline,
+    Filled,
+}
+
+enum class ArtistPresentation {
+    Covers,
+    List,
+}
+
+enum class NavIcon {
+    Home,
+    Artists,
+    Settings,
+    Covers,
+    List,
+}
